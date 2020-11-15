0c9c0217dfbbbe4e48fada8f550658d836645f17
Ilryss-Lurgorg <60524430+Ilryss-Lurgorg@users.noreply.github.com>
2020-11-15 19:10:27 +0100

    Update LivreAudio.java

--- src/musichub/business/LivreAudio.java
+++ src/musichub/business/LivreAudio.java
@@ -1,12 +1,32 @@
 package business;
 
-public class LivreAudio{
-    private String titre;
-    private String auteur;
-    private int duree;
-    private int id;
-    private int contenu;
+public class LivreAudio extends Audio
+{
+	private int contenu;
     private Langue langue;
     private Categorie categorie;
 
+    public LivreAudio(String titre, String artiste, int duree, int id, int contenu, Langue langue, Categorie categorie)
+    {
+    	this.titre = titre;
+    	this.artiste = artiste;
+    	this.duree = duree;
+    	this.id = id;
+    	this.contenu = contenu;
+    	this.langue = langue;
+    	this.categorie = categorie;
+    }
+
+    public int getContenu()
+    {
+    	return this.contenu;
+    }
+    public Langue getLangue()
+    {
+    	return this.langue;
+    }
+    public Categorie getCategorie()
+    {
+    	return this.categorie;
+    }
 }
