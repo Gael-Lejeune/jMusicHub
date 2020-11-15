c599f49ec48b9b3fb85c0a08068d438dd89d950f
Ilryss-Lurgorg <60524430+Ilryss-Lurgorg@users.noreply.github.com>
2020-11-15 19:08:05 +0100

    Update Album.java

--- src/musichub/business/Album.java
+++ src/musichub/business/Album.java
@@ -1,10 +1,25 @@
 package business;
 
-public class Album{
-    private String titre;
-    private String artiste;
-    private int duree;
-    private int dateSortie;
-    private int id;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Album extends Audio
+{
+    private int realeaseDate;
+    private ListedList<Chanson> music = new LinkedList<Chanson>();
+
+    public Album(int realeaseDate, ListedList<Chanson> music)
+    {
+    	this.realeaseDate = realeaseDate;
+    	this.music = music;
+    }
+
+    public int getRealeaseDate()
+    { //Retourne la date de sortie
+    	return this.realeaseDate;
+    }
+    public ListedList<Chanson> getMusicList()
+    {
+    	return this.music;
+    }
 }
