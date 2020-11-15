a282a4438ea59b729fcb8d2bcb94692d0fe82c76
gael-lejeune <lejeune.gael@free.fr>
2020-11-15 11:55:00 +0100

    Création du lecteur/editeur de fichier xml

--- src/musichub/main/main.java
+++ src/musichub/main/main.java
@@ -1,5 +1,26 @@
 package main;
+import business.*;
+import util.*;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.xml.sax.SAXException;
+
+import org.w3c.dom.*;
+import java.io.IOException;
+import java.io.File;
+import java.util.UUID;
 
 public class Main{
+    public static void main(String[] args) {
+        System.out.println("Welcome in jMusicHub,");
+        System.out.println("Reading library...");
+        XMLReaderWriter Reader = new XMLReaderWriter();
+        NodeList albums = null;
+        albums = Reader.parseXMLFile("files/albums.xml");
+        Reader.readAlbumsXML("files/albums.xml");
 
+    }
 }
